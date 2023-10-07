ba5817561f9e9dfde98b10b3d70443eabd7e1a5f
Mizuochi Keita <keitam913@yahoo.co.jp>
2023-10-08 03:01:42 +0900

    Make to use jwt cli

--- src/demo_github_app/jwt.zig
+++ src/demo_github_app/jwt.zig
@@ -1,47 +1,44 @@
 const std = @import("std");
-const zig_jwt = @import("../zig-jwt/jwt.zig");
 
-fn parsePEMFile(allocator: std.mem.Allocator, path: []const u8) ![]const u8 {
-    var f = try std.fs.cwd().openFile(path, .{});
-    defer f.close();
-    var pem = std.ArrayList(u8).init(std.testing.allocator);
-    defer pem.deinit();
-    var s = std.io.bufferedReader(f.reader());
-    var buf: [1024]u8 = undefined;
-    while (s.reader().readUntilDelimiter(&buf, '\n')) |line| {
-        if (std.mem.eql(u8, line[0..2], "--")) {
-            continue;
-        }
-        try pem.appendSlice(line);
-    } else |err| {
-        switch (err) {
-            error.EndOfStream => {},
-            else => return err,
-        }
+pub fn generateJWT(allocator: std.mem.Allocator, key_pem_path: []const u8, current_epoch_seconds: i64) ![]const u8 {
+    const iat_key_value = try std.fmt.allocPrint(allocator, "iat={d}", .{current_epoch_seconds - 60});
+    defer allocator.free(iat_key_value);
+    const exp_arg = try std.fmt.allocPrint(allocator, "--exp={d}", .{current_epoch_seconds + (10 * 60)});
+    defer allocator.free(exp_arg);
+    const secret_arg = try std.fmt.allocPrint(allocator, "@{s}", .{key_pem_path});
+    defer allocator.free(secret_arg);
+    const result = try std.ChildProcess.exec(.{
+        .allocator = allocator,
+        .argv = &[_][]const u8{
+            "jwt",
+            "encode",
+            "--alg",
+            "RS256",
+            "--iss",
+            "404064",
+            exp_arg,
+            "--payload",
+            iat_key_value,
+            "--secret",
+            secret_arg,
+        },
+        .max_output_bytes = 1024 * 1024,
+    });
+    errdefer allocator.free(result.stdout);
+    defer allocator.free(result.stderr);
+    if (result.term.Exited != 0) {
+        std.log.err("failed to run jwt: exit code {}", .{result.term.Exited});
+        return error.JWTCLIFailed;
     }
-    const dec = std.base64.Base64Decoder.init(std.base64.standard_alphabet_chars, '=');
-    const size = try dec.calcSizeForSlice(pem.items);
-    var b = try allocator.alloc(u8, size);
-    errdefer allocator.free(b);
-    try dec.decode(b, pem.items);
-    return b;
+    return result.stdout;
 }
 
-pub fn generateJWTFromPEMKey(allocator: std.mem.Allocator, file_path: []const u8, current_epoch_seconds: i64) ![]const u8 {
-    const key = try parsePEMFile(allocator, file_path);
-    defer allocator.free(key);
-    const token = try zig_jwt.encode(std.testing.allocator, .HS256, .{
-        .iat = current_epoch_seconds - 60,
-        .exp = current_epoch_seconds + (10 * 60),
-        .iss = "404064",
-    }, .{ .key = key });
-    errdefer allocator.free(token);
-    return token;
-}
-
-test "jwt: generateJWTFromPEMKey" {
+test "jwt: generateJWT" {
     const epoch_sec_20230101T000000Z = 1672531200;
-    const token = try generateJWTFromPEMKey(std.testing.allocator, "src/demo_github_app/mizuochik-demo-github-app.2023-10-06.private-key.pem", epoch_sec_20230101T000000Z);
+    const token = try generateJWT(std.testing.allocator, "src/demo_github_app/mizuochik-demo-github-app.2023-10-06.private-key.pem", epoch_sec_20230101T000000Z);
     defer std.testing.allocator.free(token);
-    try std.testing.expectEqualStrings("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE2NzI1MzExNDAsImV4cCI6MTY3MjUzMTgwMCwiaXNzIjoiNDA0MDY0In0._3-D3mkSjgLGRh220PQPzzKgkl5vhJwUsNyGp65_8xg", token);
+    try std.testing.expectEqualStrings(
+        "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJleHAiOjE2NzI1MzE4MDAsImlhdCI6MTY3MjUzMTE0MCwiaXNzIjo0MDQwNjR9.PWgxI3ynJUSUXboXYVOPwLf6V23pnxZ2HrUHSxoyElKMBCcMjjCqiS_wFXeuBr2QCFmPeLk_tSHL0umFqn9fd1Cq7nnu4dU9yKvhbCiaD9h76xl1fBYDwgKDIl4QAHnp1ILyjiYxxtgKKxYz2Ez8ZuIk_Fjui2Y0zgDKeaBI7YekpeHoKpizzcaKqYyRq8OV5373VBtjCneco_AT5csB-GS4j_CVzoljKGoYEMHdlWgEwS7jUCheV1cO2yEUVQE9ZQjc7du5kTD_xlRizUuuWVNMhevwYZlE-6tKYEPFKKI59Q34BHfx3ieUuplDnRJL7xgfudNnLkvGH_VKTf9XOw",
+        token,
+    );
 }
